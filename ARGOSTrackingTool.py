b3154d15e2fa97849a95dae16626f0a631d3bbc3
kelseybhusted <kelsey.husted@duke.edu>
2022-09-27 23:32:27 -0400

    Read line from file instead of copy/paste

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,8 +9,33 @@
 # Date:   Fall 2022
 #--------------------------------------------------------------
 
-#copy and paste one line of data as a variable
-lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
+#Create a variable pointing to the data file
+file_name = 'data/raw/Sara.txt'
+
+#Create a file object from the file
+file_object = open(file=file_name, mode = 'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Pretend we read or extract one line of data from the file
+lineString = line_list[200]
+
+#Split the string into a list of data items
+lineData = lineString.split()
+
+#Extract items in list into variables
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
+
+#Print the location of sara
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
 
 #Split lineString into a list of items
 lineData = lineString.split()
