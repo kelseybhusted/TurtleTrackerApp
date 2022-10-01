345729e1ce9a1c6a0ed4a1011e4095ac841fbac4
kelseybhusted <kelsey.husted@duke.edu>
2022-10-01 13:03:48 -0400

    Filtering records based on obs loc

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,32 +16,50 @@ file_name = 'data/raw/Sara.txt'
 file_object = open(file=file_name, mode = 'r')
 
 #Read contents of file into a list
-lineString = file_object.readline()
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Create empty dictionaries
+date_dict = {}
+location_dict = {}
+
 
 #Pretend we read or extract one line of data from the file
-while lineString:
+for lineString in line_list:
     
     #Check to see if the lineString is a data line
     if lineString[0] =="#" or lineString[0] == 'u':
-        lineString = file_object.readline()
         continue
 
     #Split the string into a list of data items
     lineData = lineString.split()
     
     #Extract items in list into variables
-    record_id = lineData[0] # ARGOS tracking record ID
-    obs_date = lineData[2]   # Observation date
-    ob_lc = lineData[4]       # Observation Location Class
-    obs_lat = lineData[6]     # Observation Latitude
-    obs_lon = lineData[7]     # Observation Longitude
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+    
+    #Add items to dictionaries, if lc criteria is met
+    if obs_lc in ('1', '2', '3'):
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat, obs_lon)
     
     #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-    
-    #Move to the next line in the file
-    lineString = file_object.readline()
+    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
 
-#CLose the file object
-file_object.close()
+#Split lineString into a list of items
+lineData = lineString.split()
 
+#Assign variables to items in the lineData list
+record_id = lineData[0] # ARGOS tracking record ID
+obs_date = lineData[2]   # Observation date
+ob_lc = lineData[4]       # Observation Location Class
+obs_lat = lineData[6]     # Observation Latitude
+obs_lon = lineData[7]     # Observation Longitude
+
+# Print information to the use
+print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
